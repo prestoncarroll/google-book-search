5360168dc33fe38fa0b78a4d9f9510f718e0e72f
Preston Carroll <plutusinc1@yahoo.com>
2020-03-12 14:55:00 -0500

    books are now appending on page

--- google-book-search/src/App.js
+++ google-book-search/src/App.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-// import axios from 'axios';
+import axios from 'axios';
 
 import './App.css';
 
 function App() {
+
+  const [book, setBook] = useState("")
+  const [result, setResult] = useState([])
+  const [apiKey, setApiKey] = useState("")
+  //put api key above
+
+  function handleChange(event) {
+
+    const book = event.target.value;
+    setBook(book);
+
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=avengers" + book + "&key=" + apiKey + "&maxResults=35")
+      .then(data => {
+        setResult(data.data.items);
+      })
+
+  }
+
+
   return (
     <div class="container">
       <h1> Search for books!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div class="form-group">
-          <input type="text" className="form-control"
+          <input type="text" onChange={handleChange}
+            className="form-control"
             placeholder=" search for books"
           />
         </div>
         <button type="submit" className="btn btn-danger"> Search</button>
       </form>
+
+
+      {result.map(book => (
+        <a target="_blank" href={book.volumeInfo.previewLink} >
+          <img src={book.volumeInfo.imageLinks.thumbnail}
+            alt="{book.title}" />
+        </a>
+      ))}
 
 
     </div >
