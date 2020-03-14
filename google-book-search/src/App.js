f01169e62fb346f4bf64f5f629dfa37c6bcd7038
Preston Carroll <plutusinc1@yahoo.com>
2020-03-14 16:44:14 -0500

    event.target

--- google-book-search/src/App.js
+++ google-book-search/src/App.js
@@ -1,44 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import ReactDOM from 'react-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Cart from "./components/pages/Cart";
 import Home from "./components/pages/Home";
 
-// import Cart from '..src/Cart';
+
 
 
 import './App.css';
 
+
 function App() {
-
-  // const [book, setBook] = useState("")
-  // const [result, setResult] = useState([])
-  // const [apiKey, setApiKey] = useState("AIzaSyB1-g38wEt_Oropb69u87KvY740kGkMiCc")
-  // //put api key above
-
-  // function handleChange(event) {
-
-  //   const book = event.target.value;
-  //   setBook(book);
-
-  // }
-
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-
-
-  //   axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=35")
-  //     .then(data => {
-  //       console.log(data.data.items)
-  //       setResult(data.data.items);
-  //     })
-
-  // }
-
-
   return (
-
     <Router>
       <div>
         <Route exact path="/" component={Home} />
@@ -46,42 +20,11 @@ function App() {
 
       </div>
     </Router>
-
-
-    //     <div className="container">
-    //       <h1> Search for books! </h1>
-
-    //       <form onSubmit={handleSubmit}>
-    //         <div className="form-group">
-    //           <input type="text" onChange={handleChange}
-    //             className="form-control"
-    //             placeholder=" search for books"
-    //           />
-    //         </div>
-    //         <button type="submit" className="btn btn-danger"> Search</button>
-    //       </form>
-
-
-    //       {result.map(book => (
-    //         <a target="_blank" href={book.volumeInfo.previewLink || ''} >
-    //           <img src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail ? book.volumeInfo.imageLinks.thumbnail : ''}
-    //             alt={book.title} />
+  )
+};
 
 
 
 
-    //           {/* <Link to="">Home</Link> */}
-    //           <button type="submit" className="btn btn-caution"> add to cart</button>
-    //         </a>
-
-
-    //       ))}
-
-
-
-    //     </div >
-
-  );
-}
 
 export default App;
--- google-book-search/src/components/pages/Home.js
+++ google-book-search/src/components/pages/Home.js
@@ -1,14 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Home = () => {
+
+const APIKEY = "AIzaSyB1-g38wEt_Oropb69u87KvY740kGkMiCc";
+
+
+function Home() {
+    const [book, setBook] = useState("")
+    const [books, setBooks] = useState([])
+    const [cart, setCart] = useState([])
+
+    //put api key above
+
+    function handleChange(event) {
+
+        const book = event.target.value;
+        setBook(book);
+
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (book) {
+            axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + APIKEY + "&maxResults=35")
+                .then(data => {
+                    console.log(data.data.items)
+                    setBooks(data.data.items);
+                })
+
+        }
+
+    }
+
+    function handleCart(event) {
+
+
+
+        console.log(event.currentTarget.book)
+    }
+
+
     return (
-        <div>
-            <h1>
-                home
-            </h1>
-        </div>
-    )
+
+
+        <div className="container">
+            <h1> Search for books! </h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input type="text" onChange={handleChange}
+                        className="form-control"
+                        placeholder=" search for books"
+                    />
+                </div>
+                <button type="submit" className="btn btn-danger"> Search</button>
+            </form>
+
+
+            {books.map(book => (
+
+                <div key={book.id}  >
+                    <a target="_blank" href={book.volumeInfo.previewLink || ''} >
+                        <img src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail ? book.volumeInfo.imageLinks.thumbnail : ''}
+                            alt={book.title} />
+
+    
+
+
+
+
+                        {/* <Link to="">Home</Link> */}
+                    </a>
+
+
+
+                    <button book={book} onClick={handleCart} className="btn btn-caution"> add to cart</button>
+                </div>
+
+
+
+            ))}
+
+
+
+        </div >
+
+    );
 }
 
-export default Home
+export default Home;
 
