06a9d24531f89c8c4abc1913a269f7086ddbfb7c
Preston Carroll <plutusinc1@yahoo.com>
2020-03-26 20:09:34 -0500

    Updates

---
+++ precache-manifest.63f04c3a87dc9de0f556f3f65ad6b209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3144dbf7f57d3b24f32d8097d9f5aa1c",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "88e154231adb8ccfc646",
+    "url": "/google-book-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e214bbed2658a6622b25",
+    "url": "/google-book-search/static/js/2.b9e4851f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/google-book-search/static/js/2.b9e4851f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e154231adb8ccfc646",
+    "url": "/google-book-search/static/js/main.49978b7f.chunk.js"
+  },
+  {
+    "revision": "0fd88bf20aa41606f508",
+    "url": "/google-book-search/static/js/runtime-main.6d83adc6.js"
+  }
+]);
